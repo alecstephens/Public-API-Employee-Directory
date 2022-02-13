6ea477c147c2a34c9060e527ff10be0d9c6714b6
alecstephens <stephens.alec01@gmail.com>
2022-02-13 11:22:17 -0600

    created a displayEmployees function

--- js/app.js
+++ js/app.js
@@ -12,6 +12,26 @@ fetch(urlAPI)
     .then(dispatchEmployees)
     .catch(error => console.log(error))
 
-function dispatchEmployees(employeeData) {
-    
+function displayEmployees(employeeData) {
+    employees = employeeData;
+    let employeeHTML = '';
+
+    employees.forEach((employee, index) => {
+        let name = employee.name;
+        let email = employee.email;
+        let city = employee.location.city;
+        let picture = employee.picture;
+
+        employeeHTML += `
+            <div class="card" data-index="${index}">
+                <img class="avatar" src="${picture.large}" />
+                <div class="text-container">
+                    <h2 class="name">${name.first} ${name.last}</h2>
+                    <p class="email">${email}</p>
+                    <p class="address">${city}</p>
+                </div>
+            </div>
+        `;
+    })
+    gridContainer.innerHTML = employeeData;
 }
