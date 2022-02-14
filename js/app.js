4f145e1fb16a415d4741e9c7a797ebed88679e6c
alecstephens <stephens.alec01@gmail.com>
2022-02-14 14:12:33 -0600

    finished the js and added comments

--- js/app.js
+++ js/app.js
@@ -2,20 +2,22 @@ const gridContainer = document.querySelector('.grid-container');
 const overlay = document.querySelector('.overlay');
 const modalContainer = document.querySelector('.modal-content');
 const modalClose = document.querySelector('.modal-close');
-const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
-email, location, phone, dob &noinfo &nat=US`;
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name,picture,email,location,phone,dob&noinfo&nat=US`;
 let employees = [];
 
+// fetch used to retrieve the employee information from the api
 fetch(urlAPI) 
     .then(res => res.json())
     .then(res => res.results)
-    .then(dispatchEmployees)
-    .catch(error => console.log(error))
+    .then(displayEmployees)
+    .catch(err => console.log(err))
 
+// function that will display the random employees
 function displayEmployees(employeeData) {
     employees = employeeData;
     let employeeHTML = '';
 
+    // loops through each employee
     employees.forEach((employee, index) => {
         let name = employee.name;
         let email = employee.email;
@@ -33,5 +35,42 @@ function displayEmployees(employeeData) {
             </div>
         `;
     })
-    gridContainer.innerHTML = employeeData;
+    gridContainer.innerHTML = employeeHTML;
 }
+
+
+// adds additional information about each employee
+function displayModal(index) {
+    let {name, dob, phone, email, location: {city, street, state, postcode}, picture} = employees[index];
+    let date = new Date(dob.date);
+
+    const modalHTML = `
+        <img class="avatar" src="${picture.large}" />
+        <div class="text-container">
+            <h2 class="name">${name.first} ${name.last}</h2>
+            <p class="email">${email}</p>
+            <p class="address">${city}</p>
+            <hr />
+            <p>${phone}</p>
+            <p class="address">${street.number} ${street.name}, ${state} ${postcode}</p>
+            <p>Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+        </div>
+    `;
+    overlay.classList.remove('hidden');
+    modalContainer.innerHTML = modalHTML;
+}
+
+// Event listeners
+// makes a card appear with imformation about an employee when that employee is clicked
+gridContainer.addEventListener('click', (e) => {
+    if(e.target !== gridContainer) {
+        const card = e.target.closet('.card');
+        const index = card.getAttribute('data-index');
+
+        displayModal(index);
+    }
+});
+
+modalClose.addEventListener('click', () => {
+    overlay.classList.add('hidden');
+});
